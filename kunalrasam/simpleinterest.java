d4c60f8b3eee925726f19f4fdd58772d66eb3563
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:28:10 +0530

    Create simpleinterest.java

---
+++ kunalrasam/simpleinterest.java
@@ -0,0 +1,15 @@
+package com.first_java;
+
+public class simpleinterest {
+
+	public static void main(String[] args) {
+		 float p, r,  t,  si; 
+         p = 13000;  r = 12; t = 2;  
+         si  = (p*r*t)/100;   
+         System.out.println("Simple Interest is: " +si);  
+         
+         
+
+	}
+
+}
