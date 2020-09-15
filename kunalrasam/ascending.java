b71f3b5156a3e108ce298cabec4830b1647f4f65
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:25:28 +0530

    Create ascending.java

---
+++ kunalrasam/ascending.java
@@ -0,0 +1,43 @@
+package com.first_java;
+
+import java.util.Scanner;
+
+public class ascending {
+
+	public static void main(String[] args) {
+int count, temp;
+    	
+    	//User inputs the array size
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of elements you want in the array: ");
+        count = scan.nextInt();
+    
+        int num[] = new int[count];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < count; i++) 
+        {
+            num[i] = scan.nextInt();
+        }
+        scan.close();
+        for (int i = 0; i < count; i++) 
+        {
+            for (int j = i + 1; j < count; j++) { 
+                if (num[i] > num[j]) 
+                {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        System.out.print("Array Elements in Ascending Order: ");
+        for (int i = 0; i < count ; i++) 
+        {
+            System.out.print(num[i] + ", ");
+        }
+        
+    }
+
+	}
+
+
