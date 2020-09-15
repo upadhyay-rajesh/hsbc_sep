6f35cb472ab9d411df42bc1d6b6ef56daf9fc660
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:27:22 +0530

    Create printnames.java

---
+++ kunalrasam/printnames.java
@@ -0,0 +1,22 @@
+package com.first_java;
+
+public class printnames {
+
+	public static void main(String[] args) {
+		int loop; 
+        String name="kunal"; 
+         
+        loop=1; 
+        while(loop<=10){
+            System.out.println(name);
+            loop++; 
+        }
+        loop=1;
+        while(loop<=10){
+            System.out.print(name);
+            loop++; 
+        }
+
+	}
+
+}
