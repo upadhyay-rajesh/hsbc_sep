9c3d596ba2adc70dda06343eadb4972f821edfc5
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:16:23 +0530

    Create addtwonumbers.java

---
+++ kunalrasam/addtwonumbers.java
@@ -0,0 +1,16 @@
+package com.first_java;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("enter two numbers");
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
