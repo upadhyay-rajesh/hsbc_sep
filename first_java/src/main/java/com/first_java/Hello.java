e9332b3775a870b3fd15984f0b6696aa41d89439
R2v <R2v@Rajesh>
2020-09-15 15:31:11 +0530

    fgsdg

--- first_java/src/main/java/com/first_java/Hello.java
+++ first_java/src/main/java/com/first_java/Hello.java
@@ -1,28 +1,25 @@
 package com.first_java;
 
-public class Hello {
+class Hello {
 	
 	int i;
-	static int j=6;
+	static int j;
 
 	public static void main(String[] args) {
-		j=j+1;
+		int k=9;
 		
-		System.out.println("my name is  "+j);
+		Hello h=new Hello();
+		System.out.println("value of i "+h.i);
+		System.out.println("value of j "+j);
 		
-		/*
-		 * int k=9;
-		 * 
-		 * Hello h=new Hello(); System.out.println("value of i "+h.i);
-		 * System.out.println("value of j "+j);
-		 * 
-		 * System.out.println("value of k "+k);
-		 * 
-		 * 
-		 * long l1=5; int i1=(int)l1*10;
-		 * 
-		 * int i2=9; long l2=i2*10;
-		 */
+		System.out.println("value of k "+k);
+		
+		
+		long l1=5;
+		int i1=(int)l1*10;
+		
+		int i2=9;
+		long l2=i2*10;
 
 	}
 
