00df9634a541cff3b25dab89b157cb27a5894f67
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:25:44 +0530

    Create cube.java

---
+++ kunalrasam/cube.java
@@ -0,0 +1,17 @@
+package com.first_java;
+
+public class cube {
+
+	public static void main(String[] args) {
+		System.out.println("cube of numbers from 1-10");
+		int num;
+		for(int i=1;i<=10;i++)
+		{
+			
+			num=i*i*i;
+			System.out.println("cube of"+i+"is"+num);
+			
+		}
+	}
+
+}
