027da18c12bdcddc7ece15c2a774e56368690a84
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:25:36 +0530

    Create centigrade.java

---
+++ kunalrasam/centigrade.java
@@ -0,0 +1,18 @@
+package com.first_java;
+
+import java.util.Scanner;
+
+public class centigrade {
+
+	public static void main(String[] args) {
+		float fah=69;
+        double cel;
+      
+		
+        cel = (fah-32) / 1.8;
+		
+        System.out.print("Equivalent Temperature in Celsius = " + cel);
+
+	}
+
+}
