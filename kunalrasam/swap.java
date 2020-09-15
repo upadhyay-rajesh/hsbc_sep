95867efe31e71acf4b231426e8d7935ad020cfc8
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:28:27 +0530

    Create swap.java

---
+++ kunalrasam/swap.java
@@ -0,0 +1,17 @@
+package com.first_java;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  int x = 10;  
+		  int y = 20;    
+		  x = x + y;  
+		  y = x - y;  
+		  x = x - y;  
+		  System.out.println("value of x:" + x);  
+		  System.out.println("value of y:" + y);  
+	}
+
+}
