01c1c25c3f1c27d9c7136d744c1f5830020d5867
kunalrasam <kunalrasam4@gmail.com>
2020-09-15 22:28:23 +0530

    Create table.java

---
+++ kunalrasam/table.java
@@ -0,0 +1,20 @@
+package com.first_java;
+
+public class table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number for table");
+		int num=Integer.parseInt(args[0]);
+		int num1=0;
+		
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(num1);
+			num1=num1+num;
+		
+		}
+
+	}
+
+}
