c1fb6af4df64ce329806a81c3f3e970e47e72530
Yunay-654 <71279599+Yunay-654@users.noreply.github.com>
2020-09-16 15:24:30 +0530

    update

--- Dhananjay_KumarJavaAssignment/first_java/src/main/java/Fifth.java
+++
@@ -1,9 +0,0 @@
-
-public class Fifth {
-          public static void main(String args[]) {
-        	  System.out.println("Table of 2");
-        	  for(int i=1;i<=10;i++) {
-        		  System.out.println("2*"+i+"="+2*i);
-        	  }
-          }
-}
--- first_java/src/main/java/p1/A.java
+++
@@ -1,8 +0,0 @@
-package p1;
-
-public class A {
-	int i=1;
-	public int j=2;
-	private int k=3;
-	protected int l=9;
-}
