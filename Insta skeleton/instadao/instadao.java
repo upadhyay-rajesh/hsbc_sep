ac4dfaf9a70acd532cdcc94e95745e74d9929eaa
kunalrasam <kunalrasam4@gmail.com>
2020-09-16 17:19:46 +0530

    Create instadao.java

---
+++ Insta skeleton/instadao/instadao.java
@@ -0,0 +1,24 @@
+package com.first_java.instadao;
+
+public class instadao implements instadaointerface{
+
+	@Override
+	public void daorequest() {
+		System.out.println("you have succesfully reqested to person");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void daosearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void daocomment() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
